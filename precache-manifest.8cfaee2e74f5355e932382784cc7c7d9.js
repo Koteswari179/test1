07a29190154c7cf62c9d425e3fb1332b15ce46be
harshav2101 <harshav@brightcomgroup.com>
2024-06-19 15:01:12 +0530

    Updates

---
+++ precache-manifest.8cfaee2e74f5355e932382784cc7c7d9.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8722c4cbcda11ac30536fca9f7654138",
+    "url": "/Koteswari179.github.io/test1/index.html"
+  },
+  {
+    "revision": "f397b6482efe308749bb",
+    "url": "/Koteswari179.github.io/test1/static/css/2.71fe0a9e.chunk.css"
+  },
+  {
+    "revision": "3b1f624892b120950d07",
+    "url": "/Koteswari179.github.io/test1/static/css/main.aff54be0.chunk.css"
+  },
+  {
+    "revision": "f397b6482efe308749bb",
+    "url": "/Koteswari179.github.io/test1/static/js/2.4edbb050.chunk.js"
+  },
+  {
+    "revision": "cfbc82b78d39c3c1b97e6c8e0973ca79",
+    "url": "/Koteswari179.github.io/test1/static/js/2.4edbb050.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe39be0cf2798f9d038d",
+    "url": "/Koteswari179.github.io/test1/static/js/3.76f3dfb3.chunk.js"
+  },
+  {
+    "revision": "3b1f624892b120950d07",
+    "url": "/Koteswari179.github.io/test1/static/js/main.67895618.chunk.js"
+  },
+  {
+    "revision": "84c4bb4a90f45f16d5d1",
+    "url": "/Koteswari179.github.io/test1/static/js/runtime-main.d7361142.js"
+  },
+  {
+    "revision": "ba49e844892321d8540ea3b7c088cf97",
+    "url": "/Koteswari179.github.io/test1/static/media/bootstrap-icons.ba49e844.woff"
+  },
+  {
+    "revision": "cc1e5eda776be5f0ff614285c31d4892",
+    "url": "/Koteswari179.github.io/test1/static/media/bootstrap-icons.cc1e5eda.woff2"
+  },
+  {
+    "revision": "3a0cb82447f8e9ce865a709a92f0c752",
+    "url": "/Koteswari179.github.io/test1/static/media/boxicons.3a0cb824.woff"
+  },
+  {
+    "revision": "4002d1c83d8dd40df85708c5601993e4",
+    "url": "/Koteswari179.github.io/test1/static/media/boxicons.4002d1c8.eot"
+  },
+  {
+    "revision": "878061312a371566f591f1f1a9f76a87",
+    "url": "/Koteswari179.github.io/test1/static/media/boxicons.87806131.ttf"
+  },
+  {
+    "revision": "aab73283f839e775f9ac86d642983653",
+    "url": "/Koteswari179.github.io/test1/static/media/boxicons.aab73283.woff2"
+  },
+  {
+    "revision": "b0bb967778275b356010f01219188eb0",
+    "url": "/Koteswari179.github.io/test1/static/media/boxicons.b0bb9677.svg"
+  },
+  {
+    "revision": "2959a23dda6fc65dea95feda4a2d7f09",
+    "url": "/Koteswari179.github.io/test1/static/media/remixicon.2959a23d.eot"
+  },
+  {
+    "revision": "3be39a0cfb3aec47ae51addd44b970c2",
+    "url": "/Koteswari179.github.io/test1/static/media/remixicon.3be39a0c.woff"
+  },
+  {
+    "revision": "868ebc06597095c5833459a99ec73a46",
+    "url": "/Koteswari179.github.io/test1/static/media/remixicon.868ebc06.woff2"
+  },
+  {
+    "revision": "fb428713523d565b967ffa4c1267d027",
+    "url": "/Koteswari179.github.io/test1/static/media/remixicon.fb428713.ttf"
+  },
+  {
+    "revision": "fc3a06817e23aacaf4fdbc8388cd063d",
+    "url": "/Koteswari179.github.io/test1/static/media/remixicon.fc3a0681.svg"
+  }
+]);
